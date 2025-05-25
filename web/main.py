1ff5be6e35731fec17c54ea2602b62d6e6340276
Your Name <example@example.com>
2025-05-25 17:35:53 +0200

    fix: исправлен шаблон статистики и добавлена документация

--- web/main.py
+++ web/main.py
@@ -487,8 +487,8 @@ async def debug_config():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
+        "web.main:app",
+        host=settings.web_host,
+        port=settings.web_port,
         reload=True
     ) 
